022d7064cc16bf9c3072a212ea8a7c6d84b8c77c
Vir Thakor <thakor.vir@gmail.com>
2018-03-31 15:01:14 -0400

    multiple monsters

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # A-Game-Attempt
+
+Working on a simple 2D game where player can move an icon with WASD.
+
+Goals:
+- Monsters can detect if player is nearby and move towards it
+- Player can interact with monsters (attack(s))
+- Monsters can interact with player (attack)
+- Monsters and players have characteristics
+	- armor
+	- attack range
+	- damage range
+	- effects
+- Walls on map
+- Monsters drop items
+- Players maintain an inventory
+- Players can pick up items
--- js/functions.js
+++ js/functions.js
@@ -1,257 +1,294 @@
-		class Player {
-			constructor() {
-				this.x = 1;
-				this.y = 1;
-			}
-		}
+class Player {
+	constructor() {
+		this.x = 1;
+		this.y = 1;
+	}
+}
 
-		var p = new Player();
-		var m = new Player();
+var p = new Player();
 
+var numMonsters = 3;
+var monsters = [];
 
 ///////////////////////////////////////////////////////////
 
-		var board = new Array(canvasHeight / pieceSize);
-		function clearBoard () {
-			board = new Array(canvasHeight / pieceSize);
+var board = new Array(canvasHeight / pieceSize);
 
-			for (var i = 0; i < board.length; i += 1) {
-				board[i] = new Array(canvasWidth / pieceSize)
+// reset board to empty
+function clearBoard () {
+	board = new Array(canvasHeight / pieceSize);
 
-				for (var j = 0; j < board[i].length; j += 1) {
-					board[i][j] = 0;
-				}
-			}
-			addBoardBorder()
+	for (var i = 0; i < board.length; i += 1) {
+		board[i] = new Array(canvasWidth / pieceSize)
+
+		for (var j = 0; j < board[i].length; j += 1) {
+			board[i][j] = 0;
 		}
+	}
+	addBoardBorder()
+}
 
-		function addBoardBorder() {
-			for (var i = 0; i < board.length; i += 1) {
-				for (var j = 0; j < board[i].length; j += 1) {
-					if (i == 0 || i == board.length - 1) {
-						board[i][j] = 3
-					} else if (j == 0 || j == board[i].length - 1) {
-						board[i][j] = 3
-					}
-				}
+// create a wall around the edges of the board
+function addBoardBorder() {
+	for (var i = 0; i < board.length; i += 1) {
+		for (var j = 0; j < board[i].length; j += 1) {
+			if (i == 0 || i == board.length - 1) {
+				board[i][j] = 3
+			} else if (j == 0 || j == board[i].length - 1) {
+				board[i][j] = 3
 			}
 		}
+	}
+}
 
-		function logBoard () {
-			var str = ""
-			for (var i = 0; i < board.length; i += 1) {
-				for (var j = 0; j < board[i].length; j += 1) {
-					var temp = '';
-					switch (board[i][j]) {
-						case 0:
-							temp = '.';
-							break;
-						case 1:
-							temp = '@';
-							break;
-						case 2:
-							temp = 'O';
-							break;
-						case 3:
-							temp = '#';
-							break;
-					}
-					str += temp + " ";
-				}
-				str += "\n"
-			}
-			console.log(str)
-		}
-
-		function setPiece(x, y, type) {
-			switch (type) {
-				case "player":
-					board[y][x] = 1
+// print the board into the console
+function logBoard () {
+	var str = ""
+	for (var i = 0; i < board.length; i += 1) {
+		for (var j = 0; j < board[i].length; j += 1) {
+			var temp = '';
+			switch (board[i][j]) {
+				case 0:
+					temp = '.';
 					break;
-				case "monster":
-					board[y][x] = 2
+				case 1:
+					temp = '@';
 					break;
-				default:
-					board[y][x] = 0
+				case 2:
+					temp = 'O';
+					break;
+				case 3:
+					temp = '#';
 					break;
 			}
+			str += temp + " ";
 		}
+		str += "\n"
+	}
+	console.log(str)
+}
 
-		function setBoard() {
-			clearBoard();
-			setPiece(p.x, p.y, "player")
-			moveMonster(m)
-			setPiece(m.x, m.y, "monster")
-			logBoard();
+// set the board value depending on the type
+function setPiece(x, y, type) {
+	switch (type) {
+		case "player":
+			board[y][x] = 1
+			break;
+		case "monster":
+			board[y][x] = 2
+			break;
+		default:
+			board[y][x] = 0
+			break;
+	}
+}
 
-		}
+// set all pieces on the board
+function setBoard() {
+	clearBoard();
+	setPiece(p.x, p.y, "player")
+	for (var i = 0; i < monsters.length; i++) {
+		moveMonster(monsters[i])
+		setPiece(monsters[i].x, monsters[i].y, "monster")
+	}
+	
+	logBoard();
+}
 
-		function initBoard() {
-			clearBoard();
-			setPiece(p.x, p.y, "player")
-			m.x = Math.floor( Math.random() * ((canvasWidth / pieceSize)/2 - 1) + (canvasWidth / pieceSize)/2)
-			m.y = Math.floor( Math.random() * ((canvasHeight / pieceSize) - 2) + 1)
-			console.log(m.x)
-			console.log(m.y)
-			setPiece(m.x, m.y, "monster")
-			logBoard();
-			console.log(board)
-		}
+// initialize all pieces on the board
+function initBoard() {
+	clearBoard();
+
+	setPiece(p.x, p.y, "player")
+
+	for (var i = 0; i < numMonsters; i++) {
+		var m = new Player();
+		m.x = Math.floor( Math.random() * ((canvasWidth / pieceSize)/2 - 1) + (canvasWidth / pieceSize)/2)
+		m.y = Math.floor( Math.random() * ((canvasHeight / pieceSize) - 2) + 1)
+		monsters.push(m);
+		setPiece(m.x, m.y, "monster")
+	}
+
+	logBoard();
+}
 
 
 
 ///////////////////////////////////////////////////////////
 // Player Functions
-		function isOpenPos(x, y) {
-			if (x < 0 && x >= canvasWidth / pieceSize) {
-				return false;
-			}
-			if (y < 0 && y >= canvasHeight / pieceSize) {
-				return false;
-			}
+// check if the coordinates in the board are open
+function isOpenPos(x, y) {
+	if (x < 0 && x >= canvasWidth / pieceSize) {
+		return false;
+	}
+	if (y < 0 && y >= canvasHeight / pieceSize) {
+		return false;
+	}
 
-			switch(board[y][x]) {
-				case 3: // Wall
-					return false;
-				case 2: // Monster
-					return false;
-				case 1: // Player
-					return false;
-				case 0: // Empty
-					return true;
-			}
+	switch(board[y][x]) {
+		case 3: // Wall
+			return false;
+		case 2: // Monster
+			return false;
+		case 1: // Player
+			return false;
+		case 0: // Empty
+			return true;
+	}
+}
+
+// set the board as user presses keys
+$('body').keyup(function( event ) {
+	// 
+}).keydown(function( event ) {
+	if ( event.which == 87 ) { // W
+		// up
+		if (isOpenPos(p.x, p.y-1)) {
+			p.y -= 1
 		}
+		console.log("up")
+		setBoard();
+	} else if ( event.which == 83 ) { // S
+		// down
+		if (isOpenPos(p.x, p.y+1)) {
+			p.y += 1
+		}
+		console.log("down")
+		setBoard();
+	} else if ( event.which == 65 ) { // A
+		// left
+		if (isOpenPos(p.x-1, p.y)) {
+			p.x -= 1
+		}
+		console.log("left")
+		setBoard();
+	} else if ( event.which == 68 ) { // D
+		// right
+		if (isOpenPos(p.x+1, p.y)) {
+			p.x += 1
+		}
+		console.log("right")
+		setBoard();
+	} else if ( event.which == 32 ) { // Space
+		// player makes attack
+	} else if ( event.which == 73 ) { // I
+		// 
+	} else if ( event.which == 79 ) { // O
+		// 
+	} else if ( event.which == 80 ) { // P
+		// 
+	} else if ( event.which == 77 ) { // M
+		// open menu
+	} 
 
-		$(document).ready(function () {
-			initBoard();
-		});
-			$('body').keyup(function( event ) {
-				// 
-			}).keydown(function( event ) {
-				if ( event.which == 87 ) {
-					// up
-					if (isOpenPos(p.x, p.y-1)) {
-						p.y -= 1
-					}
-					console.log("up")
-					setBoard();
-				} else if ( event.which == 83 ) {
-					// down
-					if (isOpenPos(p.x, p.y+1)) {
-						p.y += 1
-					}
-					console.log("down")
-					setBoard();
-				} else if ( event.which == 65 ) {
-					// left
-					if (isOpenPos(p.x-1, p.y)) {
-						p.x -= 1
-					}
-					console.log("left")
-					setBoard();
-				} else if ( event.which == 68 ) {
-					// right
-					if (isOpenPos(p.x+1, p.y)) {
-						p.x += 1
-					}
-					console.log("right")
-					setBoard();
-				}
-			});
+	else {
+		console.log(event.which)
+	}
+});
 
+// on page load, set the board
+$(document).ready(function () {
+	initBoard();
+});
+
+///////////////////////////////////////////////////////////
 // Monster Functions
-		function isPlayer(x, y) {
-			// console.log(x + " / " + ((canvasWidth / pieceSize)-1))
-			// console.log(y + " / " + ((canvasHeight / pieceSize)-1))
-			if (x < 1 || x > (canvasWidth / pieceSize)-1) {
-				return false;
-			}
-			if (y < 1 || y > (canvasHeight / pieceSize)-1) {
-				return false;
-			}
-			// console.log(board)
-			switch(board[y][x]) {
-				case 3: // Wall
-					return false;
-				case 2: // Monster
-					return false;
-				case 1: // Player
-					return true;
-				case 0: // Empty
-					return false;
-				default:
-					return false;
+// check if there is a player in the area
+function isPlayer(x, y) {
+	// console.log(x + " / " + ((canvasWidth / pieceSize)-1))
+	// console.log(y + " / " + ((canvasHeight / pieceSize)-1))
+	if (x < 1 || x > (canvasWidth / pieceSize)-1) {
+		return false;
+	}
+	if (y < 1 || y > (canvasHeight / pieceSize)-1) {
+		return false;
+	}
+	// console.log(board)
+	switch(board[y][x]) {
+		case 3: // Wall
+			return false;
+		case 2: // Monster
+			return false;
+		case 1: // Player
+			return true;
+		case 0: // Empty
+			return false;
+		default:
+			return false;
+	}
+}
+
+// search the radius of creature to look for a player
+function searchAround(radius, creature) {
+	var nearby = [];
+	for (var i = -radius; i < radius; i++) {
+		for (var j = -radius; j < radius; j++) {
+			// console.log((creature.x+i) + " , " + (creature.y+j))
+			if (isPlayer(creature.x+i,creature.y+j)) {
+				console.log((creature.x+i) + " , " + (creature.y+j))
+				nearby.push({
+					"x": creature.x+i, 
+					"y": creature.y+j,
+					"type": board[creature.y+j][creature.x+i]
+				})
 			}
 		}
+	}
+	return nearby;
+}
 
-		function searchAround(radius, creature) {
-			var nearby = [];
-			for (var i = -radius; i < radius; i++) {
-				for (var j = -radius; j < radius; j++) {
-					// console.log((creature.x+i) + " , " + (creature.y+j))
-					if (isPlayer(creature.x+i,creature.y+j)) {
-						console.log((creature.x+i) + " , " + (creature.y+j))
-						nearby.push({
-							"x": creature.x+i, 
-							"y": creature.y+j,
-							"type": board[creature.y+j][creature.x+i]
-						})
-					}
-				}
-			}
-			return nearby;
+// update the source's coordinates to move it towards the target
+function moveTowards(source, target) {
+	var end = source;
+	if (source[0] < target[0] && isOpenPos(source[0]+1, source[1])) {
+		end[0] += 1;
+	} else if (source[0] > target[0] && isOpenPos(source[0]-1, source[1])) {
+		end[0] -= 1;
+	} else if (source[1] < target[1] && isOpenPos(source[0], source[1]+1)) {
+		end[1] += 1;
+	} else if (source[1] > target[1] && isOpenPos(source[0], source[1]-1)) {
+		end[1] -= 1;
+	} else {
+
+	}
+
+	return end;
+}
+
+// perform movement towards player, otherwise move randomly
+function moveMonster(creature) {
+	var search = searchAround(4, creature);
+	var hasMoved = false;
+	for (var i = 0; i < search.length; i++) {
+		if (search[i].type == 1) {
+			var end = moveTowards([creature.x, creature.y], [search[i].x,search[i].y])
+			creature.x = end[0];
+			creature.y = end[1];
+			hasMoved = true;
+			return;
 		}
-
-		function moveTowards(source, target) {
-			var end = source;
-			if (source[0] < target[0] && isOpenPos(source[0]+1, source[1])) {
-				end[0] += 1;
-			} else if (source[0] > target[0] && isOpenPos(source[0]-1, source[1])) {
-				end[0] -= 1;
-			} else if (source[1] < target[1] && isOpenPos(source[0], source[1]+1)) {
-				end[1] += 1;
-			} else if (source[1] > target[1] && isOpenPos(source[0], source[1]-1)) {
+	}
+	if (!hasMoved) {
+		// if no target, move randomly
+		var end = [creature.x, creature.y]
+		var upDownOrLeftRight = Math.floor( (Math.random() * 2) + 1)
+		if (upDownOrLeftRight == 1) {
+			var upDown = Math.floor( (Math.random() * 2) + 1)
+			if (upDown == 1 && isOpenPos(end[0], end[1]+1)) {
+				end[1] += 1
+			} else if (isOpenPos(end[0], end[1]-1)) {
 				end[1] -= 1;
-			} else {
-
 			}
-
-			return end;
-		}
-
-		function moveMonster(creature) {
-			var search = searchAround(4, creature);
-			var hasMoved = false;
-			for (var i = 0; i < search.length; i++) {
-				if (search[i].type == 1) {
-					var end = moveTowards([creature.x, creature.y], [search[i].x,search[i].y])
-					creature.x = end[0];
-					creature.y = end[1];
-					hasMoved = true;
-					return;
-				}
-			}
-			if (!hasMoved) {
-				// if no target, move randomly
-				var end = [creature.x, creature.y]
-				var upDownOrLeftRight = Math.floor( (Math.random() * 2) + 1)
-				if (upDownOrLeftRight == 1) {
-					var upDown = Math.floor( (Math.random() * 2) + 1)
-					if (upDown == 1 && isOpenPos(end[0], end[1]+1)) {
-						end[1] += 1
-					} else if (isOpenPos(end[0], end[1]-1)) {
-						end[1] -= 1;
-					}
-				} else {
-					var leftRight = Math.floor( (Math.random() * 2) + 1)
-					if (leftRight == 1 && isOpenPos(end[0]+1, end[1])) {
-						end[0] += 1
-					} else if (isOpenPos(end[0]-1, end[1])) {
-						end[0] -= 1;
-					}
-				}
-
-				creature.x = end[0];
-				creature.y = end[1];
+		} else {
+			var leftRight = Math.floor( (Math.random() * 2) + 1)
+			if (leftRight == 1 && isOpenPos(end[0]+1, end[1])) {
+				end[0] += 1
+			} else if (isOpenPos(end[0]-1, end[1])) {
+				end[0] -= 1;
 			}
 		}
+
+		creature.x = end[0];
+		creature.y = end[1];
+	}
+}
